69b10d14572c35589b1599dd1972c996e495b04c
Christal Martin <50589854+cbaillar@users.noreply.github.com>
2024-08-23 16:11:24 -0400

    Add files via upload

--- src/emulator.py
+++ src/emulator.py
@@ -1,37 +1,14 @@
-"""
-Trains Gaussian process emulators.
-
-When run as a script, allows retraining emulators, specifying the number of
-principal components, and other options (however it is not necessary to do this
-explicitly --- the emulators will be trained automatically when needed).  Run
-``python -m src.emulator --help`` for usage information.
-
-Uses the `scikit-learn <http://scikit-learn.org>`_ implementations of
-`principal component analysis (PCA)
-<http://scikit-learn.org/stable/modules/generated/sklearn.decomposition.PCA.html>`_
-and `Gaussian process regression
-<http://scikit-learn.org/stable/modules/generated/sklearn.gaussian_process.GaussianProcessRegressor.html>`_.
-"""
-
+import joblib   
 import logging
-import pickle
+import dill as pickle
 
-import numpy as np
-
-try:                                      # compatibility hack: first try to import joblib the deprecated way
-    from sklearn.externals import joblib  # raises an ImportError if sklearn.externals.joblib is not found
-except ImportError:                       # if that failed, then try to import joblib the correct way
-    import joblib                         # this shouldn't fail, but if it does, run 'pip install joblib scikit-learn --user' to resolve
-
-from sklearn.decomposition import PCA
-from sklearn.gaussian_process import GaussianProcessRegressor as GPR
-from sklearn.gaussian_process import kernels
-from sklearn.preprocessing import StandardScaler
+import numpy as np                  
+import surmise
+from surmise.emulation import emulator as sur_emu
 
 from . import cachedir, lazydict
 from .reader import data_list,observables
 from .design import Design
-
 
 class _Covariance:
     """
@@ -51,270 +28,102 @@ class _Covariance:
             self._slices[obs2][subobs2]
         ]
 
-
 class Emulator:
-    """
-    Multidimensional Gaussian process emulator using principal component
-    analysis.
-
-    The model training data are standardized (subtract mean and scale to unit
-    variance), then transformed through PCA.  The first `npc` principal
-    components (PCs) are emulated by independent Gaussian processes (GPs).  The
-    remaining components are neglected, which is equivalent to assuming they
-    are standard zero-mean unit-variance GPs.
-
-    This class has become a bit messy but it still does the job.  It would
-    probably be better to refactor some of the data transformations /
-    preprocessing into modular classes, to be used with an sklearn pipeline.
-    The classes would also need to handle transforming uncertainties, which
-    could be tricky.
-
-    """
-
-    def __init__(self, system, npc=10, nrestarts=0):
+    def __init__(self, system):
         logging.info(
-            'training emulator for system %s (%d PC, %d restarts)',
-            system, npc, nrestarts
+            'training emulator for system %s',
+            system
         )
-
-        Y = []
-        self._slices = {}
+        self.system = system
         self.observables = observables
-        # Build an array of all observables to emulate.
+        self._slices = {}
+        self.Y = {}  
+        self.X = {}
+
         nobs = 0
         for obs, subobslist in self.observables:
             self._slices[obs] = {}
+            self.Y[obs] = {}  
+            self.X[obs] = {}
             for subobs in subobslist:
-                Y.append(data_list[system][obs][subobs]['Y'])
-                n = Y[-1].shape[1]
+                y = []
+                x = []
+                y.append(data_list[system][obs][subobs]['Y'])
+                x.append(data_list[system][obs][subobs]['x'])
+                n = y[-1].shape[1]
                 self._slices[obs][subobs] = slice(nobs, nobs + n)
                 nobs += n
+                self.Y[obs][subobs] = np.concatenate(y, axis=1).T
+                self.X[obs][subobs] = np.concatenate(x, axis=0).reshape(-1, 1)
 
-        Y = np.concatenate(Y, axis=1)
-        # pickle.dump(Y,open('mod_dat.p','wb'))
+        self.design = Design(system)
 
-        self.npc = npc
-        self.nobs = nobs
-        self.scaler = StandardScaler(copy=False)
-        self.pca = PCA(copy=False, whiten=True, svd_solver='full')
-
-        # Standardize observables and transform through PCA.  Use the first
-        # `npc` components but save the full PC transformation for later.
-        Z = self.pca.fit_transform(self.scaler.fit_transform(Y))[:, :npc]
-
-        # Define kernel (covariance function):
-        # Gaussian correlation (RBF) plus a noise term.
-        design = Design(system)
-
-       # design = joblib.load(filename='cache/lhs/design_s.p')
-       # maxes = np.apply_along_axis(max,0,design)
-       # mins = np.apply_along_axis(min,0,design)
-
-       # ptp = maxes - mins
-
-        ptp = design.max - design.min
-        print(ptp)
-        kernel = (
-            1. * kernels.Matern(
-                length_scale=ptp,
-                length_scale_bounds=np.outer(ptp, (.1, 10))
-            )
-            # kernels.WhiteKernel(
-            #     noise_level=.1**2,
-            #     noise_level_bounds=(.01**2, 1)
-            # )
-        )
-
-        # Fit a GP (optimize the kernel hyperparameters) to each PC.
-        self.gps = [
-            GPR(
-                kernel=kernel, alpha=0,
-                n_restarts_optimizer=nrestarts,
-                copy_X_train=False
-            ).fit(design, z)
-            for z in Z.T
-        ]
-        # print('Emulator design:')
-        # print(design.array)
-
-        # Construct the full linear transformation matrix, which is just the PC
-        # matrix with the first axis multiplied by the explained standard
-        # deviation of each PC and the second axis multiplied by the
-        # standardization scale factor of each observable.
-        self._trans_matrix = (
-            self.pca.components_
-            * np.sqrt(self.pca.explained_variance_[:, np.newaxis])
-            * self.scaler.scale_
-        )
-
-        # Pre-calculate some arrays for inverse transforming the predictive
-        # variance (from PC space to physical space).
-
-        # Assuming the PCs are uncorrelated, the transformation is
-        #
-        #   cov_ij = sum_k A_ki var_k A_kj
-        #
-        # where A is the trans matrix and var_k is the variance of the kth PC.
-        # https://en.wikipedia.org/wiki/Propagation_of_uncertainty
-
-        # Compute the partial transformation for the first `npc` components
-        # that are actually emulated.
-        A = self._trans_matrix[:npc]
-        self._var_trans = np.einsum(
-            'ki,kj->kij', A, A, optimize=False).reshape(npc, nobs**2)
-
-        # Compute the covariance matrix for the remaining neglected PCs
-        # (truncation error).  These components always have variance == 1.
-        B = self._trans_matrix[npc:]
-        self._cov_trunc = np.dot(B.T, B)
-
-        # Add small term to diagonal for numerical stability.
-        self._cov_trunc.flat[::nobs + 1] += 1e-4 * self.scaler.var_
+        self.gps = [sur_emu(x=self.X[obs][subobs], theta=self.design.array, f=self.Y[obs][subobs], method='PCGP') 
+                    for obs, subobslist in self.observables for subobs in subobslist]
 
     @classmethod
     def from_cache(cls, system, retrain=False, **kwargs):
-        """
-        Load the emulator for `system` from the cache if available, otherwise
-        train and cache a new instance.
 
-        """
         cachefile = cachedir / 'emulator' / '{}.pkl'.format(system)
 
-        # cache the __dict__ rather than the Emulator instance itself
-        # this way the __name__ doesn't matter, e.g. a pickled
-        # __main__.Emulator can be unpickled as a src.emulator.Emulator
         if not retrain and cachefile.exists():
             logging.debug('loading emulator for system %s from cache', system)
             emu = cls.__new__(cls)
-            emu.__dict__ = joblib.load(cachefile)
+            with open(cachefile, 'rb') as f:
+                emu.__dict__ = pickle.load(f)
             return emu
 
         emu = cls(system, **kwargs)
 
         logging.info('writing cache file %s', cachefile)
         cachefile.parent.mkdir(exist_ok=True)
-        joblib.dump(emu.__dict__, cachefile, protocol=pickle.HIGHEST_PROTOCOL)
+        with open(cachefile, 'wb') as f:
+            pickle.dump(emu.__dict__, f, protocol=pickle.HIGHEST_PROTOCOL)
 
         return emu
 
-    def _inverse_transform(self, Z):
-        """
-        Inverse transform principal components to observables.
+    def predict(self, Theta, return_cov=False):
 
-        Returns a nested dict of arrays.
+        gp_mean_dict = {}
+        gp_cov_dict = {}
+        cov_blocks = []
+        
+        for obs, subobslist in self.observables:
+            gp_mean_dict[obs] = {}
+            gp_cov_dict[obs] = {}
+            for subobs in subobslist:
+                # Find the corresponding gp model
+                gp_index = self.observables.index((obs, subobslist))
+                gp = self.gps[gp_index]
 
-        """
-        # Z shape (..., npc)
-        # Y shape (..., nobs)
-        Y = np.dot(Z, self._trans_matrix[:Z.shape[-1]])
-        Y += self.scaler.mean_
+                gp_pred = gp.predict(x=self.X[obs][subobs], theta=Theta)
+                gp_mean_dict[obs][subobs] = gp_pred.mean().T
+                gp_cov_dict[obs][subobs] = gp_pred.covx()
+                cov_blocks.append(gp_pred.covx())
+                cov_blocks.append(np.zeros_like(gp_pred.covx()))
+        
+        a = gp_cov_dict['R_AA']['C0']  # 70, 7, 7
+        b = np.zeros_like(a)
+        c = gp_cov_dict['R_AA']['C1']  # 70, 7, 7
+        d = np.zeros_like(c)
+    
+        top = np.concatenate([a, b], axis=2) 
+        bottom = np.concatenate([d, c], axis=2)  
 
-        return {
-            obs: {
-                subobs: Y[..., s]
-                for subobs, s in slices.items()
-            } for obs, slices in self._slices.items()
-        }
+        gp_cov_array = np.concatenate([top, bottom], axis=0)
 
-    def predict(self, X, return_cov=False, extra_std=0):
-        """
-        Predict model output at `X`.
-
-        X must be a 2D array-like with shape ``(nsamples, ndim)``.  It is passed
-        directly to sklearn :meth:`GaussianProcessRegressor.predict`.
-
-        If `return_cov` is true, return a tuple ``(mean, cov)``, otherwise only
-        return the mean.
-
-        The mean is returned as a nested dict of observable arrays, each with
-        shape ``(nsamples, n_cent_bins)``.
-
-        The covariance is returned as a proxy object which extracts observable
-        sub-blocks using a dict-like interface:
-
-        >>> mean, cov = emulator.predict(X, return_cov=True)
-
-        >>> mean['dN_dy']['pion']
-        <mean prediction of pion dN/dy>
-
-        >>> cov[('dN_dy', 'pion'), ('dN_dy', 'pion')]
-        <covariance matrix of pion dN/dy>
-
-        >>> cov[('dN_dy', 'pion'), ('mean_pT', 'kaon')]
-        <covariance matrix between pion dN/dy and kaon mean pT>
-
-        The shape of the extracted covariance blocks are
-        ``(nsamples, n_cent_bins_1, n_cent_bins_2)``.
-
-        NB: the covariance is only computed between observables and centrality
-        bins, not between sample points.
-
-        `extra_std` is additional uncertainty which is added to each GP's
-        predictive uncertainty, e.g. to account for model systematic error.  It
-        may either be a scalar or an array-like of length nsamples.
-
-        """
-        gp_mean = [gp.predict(X, return_cov=return_cov) for gp in self.gps]
-
+        gp_cov_array = gp_cov_array.transpose(1, 0, 2) 
         if return_cov:
-            gp_mean, gp_cov = zip(*gp_mean)
-
-        mean = self._inverse_transform(
-            np.concatenate([m[:, np.newaxis] for m in gp_mean], axis=1)
-        )
-
-        if return_cov:
-            # Build array of the GP predictive variances at each sample point.
-            # shape: (nsamples, npc)
-            gp_var = np.concatenate([
-                c.diagonal()[:, np.newaxis] for c in gp_cov
-            ], axis=1)
-
-            # Add extra uncertainty to predictive variance.
-            extra_std = np.array(extra_std, copy=False).reshape(-1, 1)
-            gp_var += extra_std**2
-
-            # Compute the covariance at each sample point using the
-            # pre-calculated arrays (see constructor).
-            cov = np.dot(gp_var, self._var_trans).reshape(
-                X.shape[0], self.nobs, self.nobs
-            )
-            cov += self._cov_trunc
-
-            return mean, _Covariance(cov, self._slices)
+            return gp_mean_dict, _Covariance(gp_cov_array, self._slices)
         else:
-            return mean
-
-    def sample_y(self, X, n_samples=1, random_state=None):
-        """
-        Sample model output at `X`.
-
-        Returns a nested dict of observable arrays, each with shape
-        ``(n_samples_X, n_samples, n_cent_bins)``.
-
-        """
-        # Sample the GP for each emulated PC.  The remaining components are
-        # assumed to have a standard normal distribution.
-        return self._inverse_transform(
-            np.concatenate([
-                gp.sample_y(
-                    X, n_samples=n_samples, random_state=random_state
-                )[:, :, np.newaxis]
-                for gp in self.gps
-            ] + [
-                np.random.standard_normal(
-                    (X.shape[0], n_samples, self.pca.n_components_ - self.npc)
-                )
-            ], axis=2)
-        )
-
-
+            return gp_mean_dict
+        
 emulators = lazydict(Emulator.from_cache)
-
 
 if __name__ == '__main__':
     import argparse
     from . import systems
+    
     def arg_to_system(arg):
         if arg not in systems:
             raise argparse.ArgumentTypeError(arg)
@@ -323,15 +132,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='train emulators for each collision system',
         argument_default=argparse.SUPPRESS
-    )
-
-    parser.add_argument(
-        '--npc', type=int,
-        help='number of principal components'
-    )
-    parser.add_argument(
-        '--nrestarts', type=int,
-        help='number of optimizer restarts'
     )
 
     parser.add_argument(
